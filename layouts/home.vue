bb4582efd28a756924a30c51b2d64e4a8e3948c4
HanifNAfrian <hanifnafrian12@gmail.com>
2025-08-04 22:26:07 +0700

    update sidebar

--- layouts/home.vue
+++ layouts/home.vue
@@ -14,41 +14,21 @@
                 <h1 class=" font-Manu-Consent text-2xl font-bold text-gray-800 dark:text-gray-200 underline">The Notes
                     Inc
                 </h1>
-                <NoteMenu />
-                <USeparator class="w-full" />
-
-                <h3 class="my-2 text-gray-500">Tags</h3>
-
-                <div v-if="storage.tags.length > 0" v-for="(tag, idx) in storage.tags" class="space-y-5 group">
-                    <div
-                        class="flex items-center justify-between gap-x-3 group-hover:bg-gray-200 dark:group-hover:bg-main-950 py-2 px-3 cursor-pointer mt-1">
-                        <div class="flex items-center gap-x-2">
-                            <UIcon name="lucide:tag" />
-                            <p>{{ tag.name }}</p>
-                        </div>
-                        <TagDropdown :tagName="tag.name" @checklistFilter="(a: boolean) => checkingTagFilter(a, idx)"
-                            :checkFilter="tagFilterCheck[idx]" />
-                    </div>
-                </div>
-                <div v-else-if="storage.tags.length <= 0 && storage.loadedAll >= 3">
-                    <p class="text-center text-muted">No Tags</p>
-                </div>
-                <div v-else class="space-y-3 mt-5">
-                    <div class="flex items-center gap-x-2">
-                        <USkeleton class="h-5 w-1/5" />
-                        <USkeleton class="h-5 w-4/5" />
-                    </div>
-                    <div class="flex items-center gap-x-2">
-                        <USkeleton class="h-5 w-1/5" />
-                        <USkeleton class="h-5 w-4/5" />
-                    </div>
-                </div>
+                <sidebarMenu />
             </div>
             <USeparator orientation="vertical" class="h-svh" />
         </div>
-        <USlideover side="left" v-model:open="openSide" title="Slideover with title">
+        <USlideover side="left" v-model:open="openSide">
+            <template #header>
+                <h1 class=" font-Manu-Consent text-2xl font-bold text-gray-800 dark:text-gray-200 underline">The
+                    Notes
+                    Inc
+                </h1>
+            </template>
             <template #body>
-                <h1>Hello World</h1>
+                <div class="w-full p-5">
+                    <sidebarMenu />
+                </div>
             </template>
         </USlideover>
         <div class="lg:pl-64 w-screen">
@@ -94,8 +74,6 @@ if (!storage.credential.id) {
     storage.getUserCredential();
 }
 
-const tagFilterCheck = ref<boolean[]>([]);
-
 const input = useTemplateRef('input')
 const toast = useToast()
 
@@ -106,23 +84,6 @@ defineShortcuts({
 })
 
 const query = ref('');
-
-watch(() => storage.tags, () => {
-    populateTagFilterCheck()
-})
-
-const checkingTagFilter = (param: boolean, idx: number) => {
-    populateTagFilterCheck();
-    tagFilterCheck.value[idx] = param
-}
-
-const populateTagFilterCheck = () => {
-    let tempTagFilterCheck = [];
-    for (const _ of storage.tags) {
-        tempTagFilterCheck.push(false);
-    }
-    tagFilterCheck.value = tempTagFilterCheck
-}
 
 watch(() => query.value, () => {
     useQueryFilterSearch(query.value)
